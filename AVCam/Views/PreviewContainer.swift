dce0dfc7dd0a1daf07f304b5c7a6d92c5021582b
FreeDeveloper97 <freedeveloper97@gmail.com>
2025-02-22 17:17:16 +0900

    comment PreviewContainer 주석 추가

--- AVCam/Views/PreviewContainer.swift
+++ AVCam/Views/PreviewContainer.swift
@@ -1,22 +1,21 @@
 /*
-See the LICENSE.txt file for this sample’s licensing information.
-
-Abstract:
-A view that provides a container view around the camera preview.
-*/
+ 이 샘플의 라이선스 정보는 LICENSE.txt 파일을 참조하세요.
+ 
+ 개요:
+ 카메라 미리보기 주위에 컨테이너 뷰를 제공하는 뷰.
+ */
 
 import SwiftUI
 
-// Portrait-orientation aspect ratios.
+// 세로 방향 비율
 typealias AspectRatio = CGSize
 let photoAspectRatio = AspectRatio(width: 3.0, height: 4.0)
 let movieAspectRatio = AspectRatio(width: 9.0, height: 16.0)
 
-/// A view that provides a container view around the camera preview.
+/// 카메라 미리보기 주위에 컨테이너 뷰를 제공하는 View.
 ///
-/// This view applies transition effects when changing capture modes or switching devices.
-/// On a compact device size, the app also uses this view to offset the vertical position
-/// of the camera preview to better fit the UI when in photo capture mode.
+/// 이 View는 캡처 모드 변경이나 장치 전환 시 전환 효과를 적용합니다.
+/// 컴팩트 장치 크기에서는 이 View를 사용하여 사진 캡처 모드에서 UI에 더 잘 맞도록 카메라 미리보기의 수직 위치를 조정합니다.
 @MainActor
 struct PreviewContainer<Content: View, CameraModel: Camera>: View {
     
@@ -24,11 +23,10 @@ struct PreviewContainer<Content: View, CameraModel: Camera>: View {
     
     @State var camera: CameraModel
     
-    // State values for transition effects.
+    // 전환 효과를 위한 상태 값.
     @State private var blurRadius = CGFloat.zero
     
-    // When running in photo capture mode on a compact device size, move the preview area
-    // update by the offset amount so that it's better centered between the top and bottom bars.
+    // 컴팩트 장치 크기에서 사진 캡처 모드일 때, 미리보기 영역을 오프셋만큼 이동시켜서 UI에 더 잘 맞도록 함.
     private let photoModeOffset = CGFloat(-44)
     private let content: Content
     
@@ -38,23 +36,23 @@ struct PreviewContainer<Content: View, CameraModel: Camera>: View {
     }
     
     var body: some View {
-        // On compact devices, show a view finder rectangle around the video preview bounds.
+        // 컴팩트 장치에서 비디오 미리보기 범위 주위에 뷰 파인더 사각형을 표시.
         if horizontalSizeClass == .compact {
             ZStack {
                 previewView
             }
             .clipped()
-            // Apply an appropriate aspect ratio based on the selected capture mode.
+            // 선택된 캡처 모드에 따라 적절한 종횡비를 적용.
             .aspectRatio(aspectRatio, contentMode: .fit)
-            // In photo mode, adjust the vertical offset of the preview area to better fit the UI.
+            // 사진 모드에서는 미리보기 영역의 수직 오프셋을 조정하여 UI에 더 잘 맞도록 함.
             .offset(y: camera.captureMode == .photo ? photoModeOffset : 0)
         } else {
-            // On regular-sized UIs, show the content in full screen.
+            // 일반 크기 UI에서는 미리보기를 전체 화면에 표시.
             previewView
         }
     }
     
-    /// Attach animations to the camera preview.
+    /// 카메라 미리보기에게 애니메이션을 적용.
     var previewView: some View {
         content
             .blur(radius: blurRadius, opaque: true)
